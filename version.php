6fad3be1005310723067249767abc75e8d80bd90
Allan Haggett <47408983+allanhaggett@users.noreply.github.com>
2026-03-02 17:25:21 -0800

    Update plugin version and release number

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026022701;
+$plugin->version   = 2026030201;
 $plugin->requires  = 2021051718; // Moodle 3.11+.
 $plugin->component = 'local_psaelmsync';
 $plugin->cron      = 600; // Run every 10 minutes.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4';
+$plugin->release   = '1.5';
